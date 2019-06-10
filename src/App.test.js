fada0fbe53e8f05d096f116fa5ba7cb9bc11ddda
alexpriceprogam <shadow27098@gmail.com>
2019-06-10 00:27:28 -0400

    tests for name

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+    import React from 'react';
+    import { shallow } from 'enzyme';
+    import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+    describe('App component', () => {
+      it('starts with a count of 0', () => {
+        const wrapper = shallow(<App />);
+        const text = wrapper.find('p').text();
+        expect(text).toEqual('Count: 0');
+      });
+    });
---
+++ src/App/Tests/Sidebar.test.js
@@ -0,0 +1,11 @@
+    import React from 'react';
+    import { shallow } from 'enzyme';
+    import Sidebar from '../Components/Sidebar.js';
+
+    describe('Sidebar component', () => {
+      it('it has the name of the restaraunt', () => {
+        const wrapper = shallow(<Sidebar />);
+        const headertext = wrapper.find('h1').html();
+        expect(headertext).toContain('Elmer&#x27;s Eatery');
+      });
+    });
