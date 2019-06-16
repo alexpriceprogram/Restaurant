998a41f7290559c655835925f19a7b38a4f14d62
alexpriceprogam <shadow27098@gmail.com>
2019-06-15 22:42:39 -0400

    more sidebar tests ticket 6

--- src/App/Tests/Sidebar.test.js
+++ src/App/Tests/Sidebar.test.js
@@ -8,4 +8,73 @@
         const headertext = wrapper.find('h1').html();
         expect(headertext).toContain('Elmer&#x27;s Eatery');
       });
+      it('it has the name of the restaraunt', () => {
+        const wrapper = shallow(<Sidebar />);
+        const headertext = wrapper.find('ul').html();
+        expect(headertext).toContain('Menus');
+      });
+      it('it has the name of the restaraunt', () => {
+        const wrapper = shallow(<Sidebar />);
+        const headertext = wrapper.find('ul').html();
+        expect(headertext).toContain('Information');
+      });
+      it('it has the name of the restaraunt', () => {
+        const wrapper = shallow(<Sidebar />);
+        const headertext = wrapper.find('ul').html();
+        expect(headertext).toContain('Contact');
+      });
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
